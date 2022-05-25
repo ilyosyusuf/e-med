87b60e1c4cbb4ed8d1dfcdd23f2163f810dbeecf
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-05-25 13:55:33 +0500

    components and constant files were added

---
+++ lib/core/constants/font/font_style.dart
@@ -0,0 +1,12 @@
+import 'package:emed/core/constants/font/font_size.dart';
+import 'package:flutter/material.dart';
+
+class FStyles {
+  static const TextStyle headline1s = TextStyle(fontSize: FSize.extraLarge);
+  static const TextStyle headline2s = TextStyle(fontSize: FSize.large);
+  static const TextStyle headline3s = TextStyle(fontSize: FSize.medium);
+  static const TextStyle headline4s = TextStyle(fontSize: FSize.medium);
+  static const TextStyle headline5s = TextStyle(fontSize: FSize.medium);
+  static const TextStyle headline6s = TextStyle(fontSize: FSize.small);
+  static const TextStyle headline7s = TextStyle(fontSize: FSize.extraSmall);
+}
